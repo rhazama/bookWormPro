41b33f2d607af1e8dba7cf046c07817b26a1dec5
rhazama <rhazama01@gmail.com>
2022-05-01 15:30:39 -0700

    added query_me query

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,16 @@
+export const QUERY_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        savedBooks{
+            bookId
+            authors
+            image
+            description
+            title
+            link
+        }
+    }
+}`;
