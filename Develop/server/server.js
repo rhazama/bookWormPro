ecce4856bf76421e025b05ffa39c2c77bede498a
rhazama <rhazama01@gmail.com>
2022-05-02 07:58:49 -0700

    added mongoose

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({typeDefs, resolvers, context: authMiddleware,});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +17,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname.at, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
