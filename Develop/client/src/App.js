5102de322f1fe4c5e2be1412595d5ab37092d558
rhazama <rhazama01@gmail.com>
2022-05-02 07:27:28 -0700

    created graphql api endpoint, and connected middleware

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -6,7 +6,24 @@ import Navbar from './components/Navbar';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink,} from '@apollo/client';
 import { setContent } from '@apollo/client/content';
 
+const httpLink = createHttpLink ({
+  url: '/graphql',
+});
 
+const authLink = setContent((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient ({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
